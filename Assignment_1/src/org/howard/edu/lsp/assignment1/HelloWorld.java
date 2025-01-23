cb71a7e97536a4e4449992ef3d3718e98431fc44
Geehan <altaybgeehan@gmail.com>
2025-01-23 16:59:16 -0500

    2nd time

--- Assignment_1/src/org/howard/edu/lsp/assignment1/HelloWorld.java
+++ Assignment_1/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -1,9 +1,14 @@
 package org.howard.edu.lsp.assignment1;
 
 public class HelloWorld {
+	
+	/**
+	 * Geehan Altayb
+	 * @param args
+	 */
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Hello Worold!");
 
 	}
 
