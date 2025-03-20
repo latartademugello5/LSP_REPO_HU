6488cfe233956ba31897c62929995a0fe494ff27
latartademugello5 <157780852+latartademugello5@users.noreply.github.com>
2025-03-20 18:45:34 -0400

    Create Book.java
    Add Book class for midterm question 2

---
+++ org/howard/edu/lsp/midterm/question2/Book.java
@@ -0,0 +1,21 @@
+package org.howard.edu.lsp.midterm.question2;
+
+/**
+ * BookDriver class to test the Book class implementation
+ */
+public class BookDriver {
+    
+    /**
+     * Main method to demonstrate the Book class functionality
+     * @param args Command line arguments (not used)
+     */
+    public static void main(String[] args) {
+        Book book1 = new Book("The Great Gatsby", "F. Scott Fitzgerald", "123456789", 1925);
+        Book book2 = new Book("The Great Gatsby", "F. Scott Fitzgerald", "123456789", 1925);
+        Book book3 = new Book("A Promised Land", "Barack Obama", "9780593239681", 2020);
+        
+        System.out.println(book1.equals(book2)); // true
+        System.out.println(book1.equals(book3)); // false
+        System.out.println(book1); // Output: Title: The Great Gatsby, Author: F. Scott Fitzgerald, ISBN: 123456789, Year Published: 1925
+    }
+}
