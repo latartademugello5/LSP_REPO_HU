4c361aae2ad556f810bd728176327967c232fa4c
latartademugello5 <157780852+latartademugello5@users.noreply.github.com>
2025-03-20 18:47:11 -0400

    Update Book.java
    editing the book class

--- org/howard/edu/lsp/midterm/question2/Book.java
+++ org/howard/edu/lsp/midterm/question2/Book.java
@@ -1,21 +1,112 @@
 package org.howard.edu.lsp.midterm.question2;
 
 /**
- * BookDriver class to test the Book class implementation
+ * Book class represents a book in a library
  */
-public class BookDriver {
+public class Book {
+    private String title;
+    private String author;
+    private String ISBN;
+    private int yearPublished;
     
     /**
-     * Main method to demonstrate the Book class functionality
-     * @param args Command line arguments (not used)
+     * Constructor for creating a Book object
+     * @param title The title of the book
+     * @param author The author of the book
+     * @param ISBN The ISBN number of the book (a unique identifier)
+     * @param yearPublished The year the book was published
      */
-    public static void main(String[] args) {
-        Book book1 = new Book("The Great Gatsby", "F. Scott Fitzgerald", "123456789", 1925);
-        Book book2 = new Book("The Great Gatsby", "F. Scott Fitzgerald", "123456789", 1925);
-        Book book3 = new Book("A Promised Land", "Barack Obama", "9780593239681", 2020);
+    public Book(String title, String author, String ISBN, int yearPublished) {
+        this.title = title;
+        this.author = author;
+        this.ISBN = ISBN;
+        this.yearPublished = yearPublished;
+    }
+    
+    /**
+     * Gets the title of the book
+     * @return The title of the book
+     */
+    public String getTitle() {
+        return title;
+    }
+    
+    /**
+     * Sets the title of the book
+     * @param title The title to set
+     */
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    
+    /**
+     * Gets the author of the book
+     * @return The author of the book
+     */
+    public String getAuthor() {
+        return author;
+    }
+    
+    /**
+     * Sets the author of the book
+     * @param author The author to set
+     */
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+    
+    /**
+     * Gets the ISBN of the book
+     * @return The ISBN of the book
+     */
+    public String getISBN() {
+        return ISBN;
+    }
+    
+    /**
+     * Sets the ISBN of the book
+     * @param ISBN The ISBN to set
+     */
+    public void setISBN(String ISBN) {
+        this.ISBN = ISBN;
+    }
+    
+    /**
+     * Gets the year the book was published
+     * @return The year the book was published
+     */
+    public int getYearPublished() {
+        return yearPublished;
+    }
+    
+    /**
+     * Sets the year the book was published
+     * @param yearPublished The year published to set
+     */
+    public void setYearPublished(int yearPublished) {
+        this.yearPublished = yearPublished;
+    }
+    
+    /**
+     * Overrides the equals method to compare books based on ISBN and author
+     * @param obj The object to compare with
+     * @return true if the books have the same ISBN and author, false otherwise
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
         
-        System.out.println(book1.equals(book2)); // true
-        System.out.println(book1.equals(book3)); // false
-        System.out.println(book1); // Output: Title: The Great Gatsby, Author: F. Scott Fitzgerald, ISBN: 123456789, Year Published: 1925
+        Book book = (Book) obj;
+        return ISBN.equals(book.ISBN) && author.equals(book.author);
+    }
+    
+    /**
+     * Returns a string representation of the book
+     * @return A string containing the book's title, author, ISBN, and year published
+     */
+    @Override
+    public String toString() {
+        return "Title: " + title + ", Author: " + author + ", ISBN: " + ISBN + ", Year Published: " + yearPublished;
     }
 }
