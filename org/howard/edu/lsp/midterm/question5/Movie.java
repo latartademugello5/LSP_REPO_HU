6af0bc2f987fa5232577b36d1b177524565527b7
latartademugello5 <157780852+latartademugello5@users.noreply.github.com>
2025-03-20 19:14:48 -0400

    Create Movie.java

---
+++ org/howard/edu/lsp/midterm/question5/Movie.java
@@ -0,0 +1,39 @@
+package org.howard.edu.lsp.midterm.question5;
+
+/**
+ * Movie class represents a movie in the streaming service
+ */
+public class Movie implements Streamable {
+    private String title;
+    
+    /**
+     * Constructor to initialize a Movie object with a title
+     * @param title The title of the movie
+     */
+    public Movie(String title) {
+        this.title = title;
+    }
+    
+    @Override
+    public void play() {
+        System.out.println("Playing movie: " + title);
+    }
+    
+    @Override
+    public void pause() {
+        System.out.println("Paused movie: " + title);
+    }
+    
+    @Override
+    public void stop() {
+        System.out.println("Stopped movie: " + title);
+    }
+    
+    /**
+     * Rewind the movie by a specified number of seconds
+     * @param seconds The number of seconds to rewind
+     */
+    public void rewind(int seconds) {
+        System.out.println("Rewinding movie: " + title + " by " + seconds + " seconds");
+    }
+}
